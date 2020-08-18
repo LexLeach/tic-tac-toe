e556e33939247cbb21b0a2372176725c0d502775
Lex <55722511+LexLeach@users.noreply.github.com>
2020-08-18 12:02:44 -0400

    added player, and click loop
    added js for players(x,0) and created a function with a loop to alternate moves for each player, added event listener to add x and o to board.

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 // 3 4 5
 // 6 7 8
 //
-//
-//
-//
-//
-//
+//1. vars
+//2. loop for clicks to determine box and player
+//3. check possible wins
+//4. log winner
+//5. reset box for another round
 //
 console.log('Beam me up, Scotty!')
 
 const board = document.getElementById('board');
 const boxs = board.querySelectorAll('.box');
+const boxsArray = Array.from(boxs);
 const possibleWins = [
   [0, 1, 2],
   [3, 4, 5],
@@ -22,3 +23,35 @@ const possibleWins = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+const players = ['x', 'o'];
+
+let currentPossition = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 0;
+let cp = players[currentPlayer];
+let turnsTaken = 0;
+
+let start = () => {
+  for (let i = 0; i < boxsArray.length; i++) {
+    const box = boxsArray[i];
+    box.addEventListener('click', (e) => {
+      const isClicked = e.target.classList.contains('x') || e.target.classList.contains('o');
+      if (!isClicked) {
+        currentMove(e.target, i);
+        changePlayer();
+      }
+    });
+  }
+}
+
+let currentMove = (self, i) => {
+  self.classList.add(cp);
+  currentPossition[i] = cp;
+  turnsTaken++;
+}
+
+let changePlayer = () => {
+  currentPlayer = 1 - currentPlayer;
+  cp = players[currentPlayer];
+}
+
+start()
